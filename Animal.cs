d33a45fc2775cdfb0e654d9e4f49dfa70966b48b
Trần Như Sum <transum30052005@gmail.com>
2024-03-17 15:55:04 +0700

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,32 @@
+﻿namespace bai1_w07
+{
+    public abstract class Animal
+    {
+        public abstract string MakeSound();
+    }
+
+    public class Tiger : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Tiger: roarrrrr!";
+        }
+    }
+    public class Chicken : Animal, Edible
+    {
+        public override string MakeSound()
+        {
+            return "Chicken: cluck-cluck!";
+        }
+
+        public string HowToEat()
+        {
+            return "could be fried";
+        }
+    }
+    public interface Edible
+    {
+        string HowToEat();
+    }
+
+}
---
+++ Fruit.cs
@@ -0,0 +1,21 @@
+﻿namespace bai1_w07
+{
+    public abstract class Fruit : Edible
+    {
+        public abstract string HowToEat();
+    }
+    public class Apple : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Apple could be slided";
+        }
+    }
+    public class Orange : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Orange could be juiced";
+        }
+    }
+}
